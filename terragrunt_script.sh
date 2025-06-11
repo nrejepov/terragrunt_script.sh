d6a6c2ba2198d64499676b9fab210dde4f7947be
Naz Rejepov <nazar.rejepov@qa.com>
2025-06-11 15:11:28 +0100

    Update terragrunt_script.sh

--- terragrunt_script.sh
+++ terragrunt_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Replace Resource Group Name
-find "/src/terraformlab" -name "*.tf" | xargs sed -i "s/REPLACEME/$resource_group/g"
-find "/src/terraformlab" -name "*.hcl" | xargs sed -i "s/REPLACEME/$resource_group/g"
-find "/src/terraformlab" -name "*.yaml" | xargs sed -i "s/REPLACEME/$resource_group/g"
+find "/src/terraformlab" -name "*.tf" | xargs sed -i "s/REPLACEME/$2/g"
+find "/src/terraformlab" -name "*.hcl" | xargs sed -i "s/REPLACEME/$2/g"
+find "/src/terraformlab" -name "*.yaml" | xargs sed -i "s/REPLACEME/$2/g"
 
 # Replace Storage Account Name
-find "/cloudacademy" -name "*.hcl" | xargs sed -i "s/STORAGEACCOUNT/$sa/g"
+find "/src/terraformlab" -name "*.hcl" | xargs sed -i "s/STORAGEACCOUNT/$3/g"
 
 exec tail -f /dev/null
